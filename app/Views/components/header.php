9ac42b5efdbfb4ca27fb6da5320317dfcb2f913a
piwnyd <piwnyd16@gmail.com>
2025-04-15 01:24:22 +0700

    week6

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -45,9 +45,24 @@
             <!-- Profile Icon -->
             <li class="nav-item dropdown pe-3">
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                    <img src="<?= base_url()?>NiceAdmin/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-                    <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+                    <img src="NiceAdmin/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+                    <span class="d-none d-md-block dropdown-toggle ps-2"><?= session()->get('username'); ?> (<?= session()->get('role'); ?>)</span>
                 </a>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow-profile">
+                    <li class="dropdown-header">
+                        <h6>Kevin Anderson</h6>
+                        <span>Web Designer</span>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="logout">
+                            <i class="bi bi-box-arrow-in-right"></i>
+                            <span>Sign Out</span>
+                        </a>
+                    </li>
+                </ul>
             </li>
         </ul>
     </nav>
